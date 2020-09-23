daf6df5fe2b4e77acae454f1e8d47ad86672d2ca
Campbell Charlton <campbell.charlton@newcastle.ac.uk>
2020-09-23 14:29:19 +0100

    Add Ping Module
    Add some cool ping stuff, also API pings.

---
+++ ping/ping.py
@@ -0,0 +1,71 @@
+import time
+import discord
+
+from redbot.core import checks, Config, commands
+from redbot.core.utils import chat_formatting as chat
+
+old_ping = None
+
+class Ping(commands.Cog):
+    """Reply with latency of bot"""
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    def cog_unload(self):
+        global old_ping
+        if old_ping:
+            try:
+                self.bot.remove_command("ping")
+            except:
+                pass
+            self.bot.add_command(old_ping)
+
+    @commands.command()
+    @commands.has_permissions(embed_links=True)
+    @commands.cooldown(rate=1, per=2, type=commands.BucketType.user)
+    async def ping(self, ctx):
+        """Reply with latency of bot."""
+        latency = self.bot.latency * 1000
+        emb = discord.Embed(title="Pong!", color=discord.Color.red())
+        emb.add_field(
+            name="Discord API", value=chat.box(str(round(latency)) + " ms"),
+        )
+        emb.add_field(name="Message", value=chat.box("…"))
+        emb.add_field(name="Typing", value=chat.box("…"))
+        # Thanks preda and fixator for the good improvements done with this command.
+        if len(self.bot.latencies) > 1:
+            # The chances of this in near future is almost 0, but who knows, what future will bring to us?
+            shards = [
+                f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)}ms"
+                for shard, pingt in self.bot.latencies
+            ]
+            emb.add_field(name="Shards", value=chat.box("\n".join(shards)))
+        # in future this may be removed or changed with improvements, if you wanna keep it as it is, pin it in your cog list.
+        before = time.monotonic()
+        message = await ctx.send(embed=emb)
+        ping = (time.monotonic() - before) * 1000
+        emb.colour = await ctx.embed_color()
+        emb.set_field_at(
+            1,
+            name="Message",
+            value=chat.box(
+                str(int((message.created_at - ctx.message.created_at).total_seconds() * 1000))
+                + " ms"
+            ),
+        )
+        emb.set_field_at(2, name="Typing", value=chat.box(str(round(ping)) + " ms"))
+
+        await message.edit(embed=emb)
+
+def setup(bot):
+    ping = Ping(bot)
+    global old_ping
+    old_ping = bot.get_command("ping")
+    if old_ping:
+        bot.remove_command(old_ping.name)
+    bot.add_cog(ping)
