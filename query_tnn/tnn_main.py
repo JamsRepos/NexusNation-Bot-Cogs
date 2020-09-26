16dda0bf0560645d16ef4805ffa03d1619c86da4
Lubricant Jam <LubricantJam@users.noreply.github.com>
2020-09-26 21:18:36 +0100

    Added Surf & Bhop Stats. (#1)
    * Added base surf stats.
    
    * Added debug.
    
    * Added database define.
    
    * Added better syntax.
    
    * Added more debug.
    
    * oops.
    
    * Further debugging.
    
    * Test 2.0
    
    * Changed string to int.
    
    * Trying TTT query.
    
    * Maybe this was the issue?
    
    * Added additional styles.
    
    * Check style before member.
    
    * Changed to equals.
    
    * Added Top 10.
    
    * Added variables for all values.
    
    * Added back lastseen.
    
    * Added back this shit.
    
    * ???????????????????
    
    * Test?
    
    * Revert "Test?"
    
    This reverts commit fd8b8344abc3a715ca18f38aa42d7c18f408803e.
    
    * Maybe dis fix?
    
    * Think it's fixed now.
    
    * Added more stats.
    
    * Removed map improvement.
    
    * :)
    
    * Formatting.
    
    * Removed from top 2.
    
    * Try another way.
    
    * Set all to inline?
    
    * Lets try a thumbnail instead.
    
    * Maybe this fixes it?
    
    * Added Last Online.
    
    * Added First Joined.
    
    * Potential inline fix?
    
    * Added two new fields and formatting.
    
    * Removed commas.
    
    * Maybe fix?
    
    * See if this is breaking it?
    
    * This should fix it.
    
    * Changed some shit.
    
    * :)
    
    * Added Country maybe?
    
    * Changed the ordering slightly.
    
    * WANKER
    
    * Added style strings.
    
    * Fixed slow motion.
    
    * Added BHOP stats.
    
    * Generalised it.
    
    * test fix
    
    * .
    
    * .
    
    * .
    
    * .
    
    Co-authored-by: Raff <raffsimms23@gmail.com>

--- query_tnn/tnn_main.py
+++ query_tnn/tnn_main.py
@@ -120,6 +120,77 @@ async def tttStats(ctx, stats, member):
     )
     await ctx.send(embed=embed)
 
+async def surfbhopStats(ctx, stats, member):
+    points = stats[0]
+    wrpoints = stats[1]
+    top10points = stats[2]
+    finishedmapspro = stats[3]
+    finishedbonuses = stats[4]
+    finishedstages = stats[5]
+    wrs = stats[6]
+    wrbs = stats[7]
+    wrcps = stats[8]
+    top10s = stats[9]
+    lastseen = datetime.datetime.fromtimestamp(int(stats[10])).strftime('%d-%m-%Y @ %H:%M:%S')
+    joined = datetime.datetime.fromtimestamp(int(stats[11])).strftime('%d-%m-%Y @ %H:%M:%S')
+    timealive = datetime.timedelta(seconds=stats[13])
+    connections = stats[13]
+    country = stats[14]
+    country = country.replace("The ","")
+    style = stats[15]
+    styles = ["Normal", "Sideways", "Half-Sideways", "Backwards", "Low-Gravity", "Slow Motion", "Fast Forward", "Freestyle"]    
+
+    if wrpoints > 0:
+        top10string = f"{top10s} - [{top10points}+{wrpoints}]"
+    else:
+        top10string = f"{top10s} - [{top10points}]"
+    
+    embed = discord.Embed(
+        timestamp = datetime.datetime.utcnow(),
+        colour=0xff0000
+    )
+    embed.set_author(
+        name=f"{member} {styles[style]} Stats",
+        icon_url="https://cdn.discordapp.com/icons/269912749327253504/08d4ddc1e97d0314de83196806bb1f9c.webp?size=128"
+    )
+    embed.add_field(
+        name="Total Points",
+        value=points
+    )
+    embed.add_field(
+        name="Top 10 Records",
+        value=top10string
+    )
+    embed.add_field(
+        name="Country",
+        value=country
+    )
+    embed.add_field(
+        name="First Joined",
+        value=joined
+    )
+    embed.add_field(
+        name="Last Online",
+        value=lastseen
+    )
+    embed.add_field(
+        name="Time Played",
+        value=timealive
+    )
+    embed.add_field(
+        name="Records",
+        value=f"Map WR: {wrs}\nStage WR: {wrcps}\nBonus WR: {wrbs}"
+    )
+    embed.add_field(
+        name="Completed",
+        value=f"Maps: {finishedmapspro}\nStages: {finishedstages}\nBonuses: {finishedbonuses}"
+    )
+    embed.add_field(
+        name="Total Connections",
+        value=connections
+    )
+    await ctx.send(embed=embed)
+
 class QueryTNN(commands.Cog):
     __author__ = "Raff"
     __version__ = "1.0.0"
@@ -195,8 +266,8 @@ class QueryTNN(commands.Cog):
         if ctx.invoked_subcommand is None:
             pass
     
-    @ttt_commands.command()
-    async def stats(self, ctx, member: discord.Member = None):
+    @ttt_commands.command(name="stats")
+    async def ttt_stats(self, ctx, member: discord.Member = None):
         if member is None:
             member = ctx.author
         try:
@@ -216,9 +287,111 @@ class QueryTNN(commands.Cog):
                     result = cursor.fetchone()
                     await tttStats(ctx, result, member)
                 except Exception as e:
-                    await ctx.send(f"> TTT stats for {member.mention} not found")
+                    await ctx.send(f"> TTT stats for {member.mention} not found.")
 
 
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+    @commands.group(name="surf")
+    async def surf_commands(self, ctx):
+        if ctx.invoked_subcommand is None:
+            pass
+    
+    @surf_commands.command(name="stats")
+    async def surf_stats(self, ctx, style = None, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+        if style is None:
+            prefix = ctx.prefix
+            await ctx.send(f"> No style specified, please use ``n``, ``sw``, ``hsw``, ``bw``, ``lg``, ``sm``, ``ff``, ``fs``.\n> Example: ``{prefix}surf stats hsw``.")
+            return
+        if style == "n":
+            styleint = 0
+        if style == "sw":
+            styleint = 1
+        if style == "hsw":
+            styleint = 2
+        if style == "bw":
+            styleint = 3
+        if style == "lg":
+            styleint = 4
+        if style == "sm":
+            styleint = 5
+        if style == "ff":
+            styleint = 6
+        if style == "fs":
+            styleint = 7
+        try:
+            connection = mysql.connector.connect(
+                host='localhost',
+                database='discord_integration',
+                user='webserver',
+                password=self.pwd)
+            if connection.is_connected():
+                names = []
+                cursor = connection.cursor()
+                try:
+                    cursor.execute(f"SELECT `steamid` FROM `du_users` WHERE userid = '{member.id}';")
+                    result = cursor.fetchone()
+                    community_id = result[0]
+                    cursor.execute(f"SELECT points, wrpoints, top10points, finishedmapspro, finishedbonuses, finishedstages, wrs, wrbs, wrcps, top10s, lastseen, joined, timealive, connections, country, style FROM `surf`.`ck_playerrank` WHERE steamid = '{community_id}' AND style = {styleint};")
+                    result = cursor.fetchone()
+                    await surfbhopStats(ctx, result, member)
+                except Exception as e:
+                    await ctx.send(f"> Surf stats for {member.mention} not found.")
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+    @commands.group(name="bhop")
+    async def bhop_commands(self, ctx):
+        if ctx.invoked_subcommand is None:
+            pass
+    
+    @bhop_commands.command(name="stats")
+    async def bhop_stats(self, ctx, style = None, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+        if style is None:
+            prefix = ctx.prefix
+            await ctx.send(f"> No style specified, please use ``n``, ``sw``, ``hsw``, ``bw``, ``lg``, ``sm``, ``ff``, ``fs``.\n> Example: ``{prefix}bhop stats hsw``.")
+            return
+        if style == "n":
+            styleint = 0
+        if style == "sw":
+            styleint = 1
+        if style == "hsw":
+            styleint = 2
+        if style == "bw":
+            styleint = 3
+        if style == "lg":
+            styleint = 4
+        if style == "sm":
+            styleint = 5
+        if style == "ff":
+            styleint = 6
+        if style == "fs":
+            styleint = 7
+        try:
+            connection = mysql.connector.connect(
+                host='localhost',
+                database='discord_integration',
+                user='webserver',
+                password=self.pwd)
+            if connection.is_connected():
+                names = []
+                cursor = connection.cursor()
+                try:
+                    cursor.execute(f"SELECT `steamid` FROM `du_users` WHERE userid = '{member.id}';")
+                    result = cursor.fetchone()
+                    community_id = result[0]
+                    cursor.execute(f"SELECT points, wrpoints, top10points, finishedmapspro, finishedbonuses, finishedstages, wrs, wrbs, wrcps, top10s, lastseen, joined, timealive, connections, country, style FROM `bhop`.`ck_playerrank` WHERE steamid = '{community_id}' AND style = {styleint};")
+                    result = cursor.fetchone()
+                    await surfbhopStats(ctx, result, member)
+                except Exception as e:
+                    await ctx.send(f"> Bhop stats for {member.mention} not found.")
 
         except Error as e:
             print("Error while connecting to MySQL", e)
