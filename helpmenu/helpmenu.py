329c169c02fac4b22daacc3a856988e99e5dfeb7
Lubricant Jam <LubricantJam@users.noreply.github.com>
2020-10-01 14:55:51 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/NexusHub/Bot-Cogs""
    This reverts commit b7e12fc53d462b583c551634f16e01da1ca94ac6.

--- helpmenu/helpmenu.py
+++ helpmenu/helpmenu.py
@@ -76,8 +76,8 @@ class HelpMenu(commands.Cog):
         generalcommands = """`ping` `avatar` `snapchat` `invite` `contact` `prefix` `stats` `userinfo` `serverinfo`"""
         moderationcommands = """`prune` `cleanup` `ban` `hackban` `ignore` `kick` `modset` `mute` `names` `rename` `slowmode` `softban` `tempban` `unban` `unignore` `unmute` `modlogsetup` `modlogset` `case` `casefor` `reason`"""
         musiccommands = """`audioset` `audiostats` `bump` `disconnect` `summon` `local` `np` `pause` `percent` `play` `playlist` `prev` `queue` `remove` `repeat` `search` `seek` `shuffle` `skip` `stop` `volume` `lyrics`"""
-        nexuscommands = """`jb` `ttt`"""
-        utilitycommands = """`starboard` `customcom`  `filter` `filterset` `autorole` `alias` `google` `away` `toggleaway` `gif` `imgtfy` `imgur` `covid` `color`"""
+        nexuscommands = """`jb` `ttt` `surf` `bhop`"""
+        utilitycommands = """`starboard` `customcom`  `filter` `filterset` `autorole` `alias` `google` `away` `toggleaway` `gif` `imgtfy` `imgur` `covid` `color` `autoroom` `autoroomset`"""
         page2 = discord.Embed(colour=(await ctx.embed_colour()))
         page2.set_author(name=title, icon_url=ctx.bot.user.avatar_url)
         # page2.set_thumbnail(url=ctx.bot.user.avatar_url)
